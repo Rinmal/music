15d9c085bf16edc57d1dd53f699887e929855bbc
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-03-12 09:41:51 +0000

    ゲストログイン機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,7 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit]
+  before_action :ensure_guest_user, only: [:edit]
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -6,5 +9,29 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "プロフィールを更新しました"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "ゲストユーザー"
+      redirect_to user_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません'
+    end
+  end
+
 end
