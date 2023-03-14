7159e040a915ee6027ad95eb5f554161f723e661
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-03-14 03:19:39 +0000

    管理者->グループ

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -2,8 +2,21 @@ class Admin::GroupsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @groups = Group.all
   end
 
   def show
+    @group = Group.find(params[:id])
+    @users = @group.users
   end
+
+  def chat
+    @group = Group.find_by(id: params[:id])
+    if @group.present?
+      @messages = @group.messages
+    else
+      redirect_to posts_path
+    end
+  end
+
 end
--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,2 +1,8 @@
 class Admin::MessagesController < ApplicationController
+
+  def destroy
+    @message = Message.find(params[:id]).destroy
+    redirect_back fallback_location: root_path
+  end
+
 end
