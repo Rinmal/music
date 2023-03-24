1cda2f4344a7f7bc99e8f974d8f9d136c4f159d4
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-03-24 11:04:04 +0000

    ページネーション

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,6 @@ Rails.application.routes.draw do
 
   scope module: :public do
     root to: 'homes#top'
-    get 'about' => 'homes#about'
 
     resources :posts, except: [:edit, :update] do
       resource :favorites, only: [:create, :destroy]
